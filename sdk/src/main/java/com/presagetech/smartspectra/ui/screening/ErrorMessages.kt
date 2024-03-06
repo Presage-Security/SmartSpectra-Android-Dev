e221225707c1d1ff8a92a2415de91070d0d6ab7a
Jim Winkelmann <jim@presagetech.com>
2024-03-06 17:02:56 -0600

    fix error strings: updated the error strings a bit more

--- sdk/src/main/java/com/presagetech/smartspectra/ui/screening/ErrorMessages.kt
+++ sdk/src/main/java/com/presagetech/smartspectra/ui/screening/ErrorMessages.kt
@@ -8,7 +8,7 @@ object ErrorMessages {
         0 to R.string.all_good,
         1 to R.string.no_face_found,
         2 to R.string.too_many_face,
-        3 to R.string.face_is_not_centered,
+        3 to R.string.face_distance,
         4 to R.string.face_is_not_centered,
         5 to R.string.image_too_dark,
         6 to R.string.image_too_bright,
