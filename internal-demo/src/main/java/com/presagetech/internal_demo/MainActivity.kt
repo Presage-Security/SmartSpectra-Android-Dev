8e0b7920752a00a04d37bd76ef7db5f44fc6991f
Jim Winkelmann <jim@presagetech.com>
2024-04-16 21:38:01 -0500

    chore: commenting usage

--- internal-demo/src/main/java/com/presagetech/internal_demo/MainActivity.kt
+++ internal-demo/src/main/java/com/presagetech/internal_demo/MainActivity.kt
@@ -6,14 +6,19 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
+import timber.log.Timber
+
+// Plotting imports
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+
+// SmartSpectra SDK Specific Imports
 import com.presagetech.smartspectra.SmartSpectraButton
 import com.presagetech.smartspectra.SmartSpectraResultView
-import org.json.JSONObject
-import timber.log.Timber
+
 
 class MainActivity : AppCompatActivity(), SmartSpectraResultView.SmartSpectraResultsCallback {
     private lateinit var tokenEditText: EditText
@@ -50,44 +55,82 @@ class MainActivity : AppCompatActivity(), SmartSpectraResultView.SmartSpectraRes
             Timber.d("This device ABIs: ${Build.SUPPORTED_ABIS.contentToString()}")
         }
     }
+
+    /**
+     * Receives JSON metrics from the measurement (see readme for JSON structure).
+     * @param jsonMetrics The complete JSON object containing various metrics.
+     */
     override fun onMetricsJsonReceive(jsonMetrics: JSONObject) {
         // Here you can handle the received Metrics JSON
-        plethPlotting(jsonMetrics)
+        // Pulse Pleth Example
+        // Extract Data
+        val plethPulseJson = jsonMetrics.getJSONObject("pulse").getJSONObject("hr_trace")
+        // Parse and Sort the data
+        val plethEntries = parseAndSortEntries(plethPulseJson)
+        // Plot the Data
+        dataPlotting(plethEntries)
         Timber.d("Received JSON data: $jsonMetrics")
     }
+
+    /**
+     * Recieves the strict pulse rate from the measurement.
+     * Strict pulse rates are the average of only high signal to noise pulse rate values.
+     * @param strictPulseRate The strict pulse rate received, measured in beats per minute.
+     */
     override fun onStrictPuleRateReceived(strictPulseRate: Int) {
         // Here you can handle the received strict Pulse Rate in beats per minute
         Timber.d("Received JSON data: $strictPulseRate")
     }
+
+    /**
+     * Recieves the strict breathing rate from the measurement.
+     * Strict breathing rates are the average of only high confidence breathing rate values.
+     * @param strictBreathingRate The strict breathing rate received, measured in beats per minute.
+     */
     override fun onStrictBreathingRateReceived(strictBreathingRate: Int) {
         // Here you can handle the received strict Breathing Rate in beats per minute
         Timber.d("Received JSON data: $strictBreathingRate")
     }
 
-    private fun plethPlotting(jsonMetrics: JSONObject) {
-        val plethJson = jsonMetrics.getJSONObject("pulse").getJSONObject("hr_trace")
+    /**
+     * Parses a JSON object containing time and values into a sorted list of entries.
+     * @param json The JSON object where each key is a timestamp and each value is a nested JSON object with a "value" key.
+     * @return A sorted list of entries for use in a chart.
+     */
+    fun parseAndSortEntries(json: JSONObject): List<Entry> {
         val entries = mutableListOf<Entry>()
 
-        val iterator = plethJson.keys()
+        val iterator = json.keys()
         while (iterator.hasNext()) {
             val time = iterator.next() // This is the string key
-            val value = plethJson.getJSONObject(time).getDouble("value")
+            val value = json.getJSONObject(time).getDouble("value")
             entries.add(Entry(time.toFloat(), value.toFloat()))
         }
 
         // Sort the entries based on the X value (time)
         entries.sortBy { it.x }
 
+        return entries
+    }
 
-        val dataSet = LineDataSet(entries, "Pleth")
+
+    /**
+     * Configures and displays a line chart with the provided data entries.
+     * This function sets up the line chart to show a simplified and clean visualization,
+     * removing unnecessary visual elements like grid lines, axis lines, labels, and legends.
+     * It sets the line color to red and ensures that no markers or value texts are shown.
+     *
+     * @param entries The list of Entry objects representing the data points to be plotted.
+     */
+    private fun dataPlotting(entries: List<Entry>) {
+        val dataSet = LineDataSet(entries, "Data")
 
         // Clean up line
         dataSet.setDrawValues(false)
         dataSet.setDrawCircles(false)
         dataSet.color = Color.RED
-
-
         val lineData = LineData(dataSet)
+
         // clean up chart
         val xAxis = chart.xAxis
         xAxis.setDrawGridLines(false)
